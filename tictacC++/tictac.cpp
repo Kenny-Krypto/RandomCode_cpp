6d3ccd0185aa1dd5cab81df9d998565908bce749
Kenny K. Bui <56941890+Kenny-Krypto@users.noreply.github.com>
2019-10-23 20:05:36 -0400

    V2
    readablity, win condition addition if -> switch

--- tictacC++/tictac.cpp
+++ tictacC++/tictac.cpp
@@ -31,7 +31,8 @@ int GameCondition = 0, player = 1, Action = 0, wincondi[8]={0},i,turns = 0;
 char positions[10] = "123456789";
 while(GameCondition != -1)
 {
-	if(GameCondition == 3) {reset(positions);GameCondition = 0;turns = 0;} //reset condition
+	if(GameCondition == 3) 
+		{reset(positions);GameCondition = 0;turns = 0;} //reset condition
 	else if(GameCondition == 0) //normal game condition
 	{
 		tictacdisp(positions);
@@ -57,59 +58,54 @@ while(GameCondition != -1)
 		for(i = 0; i<8; i++) wincondi[i]=0; // reset win conditions
 		for(i = 0; i<9; i++) // V1 V2 V3 H1 H2 H3 D1 D2 <- win conditions vertical and horiznal diag. 3 for player one to win -3 for player 2
 		{
-			if(i == 0)
+			switch(i)
 			{
-				wincondi[0]+=checkslot(positions,i);
-				wincondi[3]+=checkslot(positions,i);
-				wincondi[6]+=checkslot(positions,i);
-			}
-			else if(i == 1)
-			{
-				wincondi[1]+=checkslot(positions,i);
-				wincondi[3]+=checkslot(positions,i);
-			}
-			else if(i == 2)
-			{
-				wincondi[2]+=checkslot(positions,i);
-				wincondi[3]+=checkslot(positions,i);
-				wincondi[7]+=checkslot(positions,i);
-			}
-			else if(i == 3)
-			{
-				wincondi[0]+=checkslot(positions,i);
-				wincondi[4]+=checkslot(positions,i);
-			}
-			else if(i == 4)
-			{
-				wincondi[1]+=checkslot(positions,i);
-				wincondi[4]+=checkslot(positions,i);
-				wincondi[6]+=checkslot(positions,i);
-				wincondi[7]+=checkslot(positions,i);
-			}
-			else if(i == 5)
-			{
-				wincondi[2]+=checkslot(positions,i);
-				wincondi[4]+=checkslot(positions,i);
-			}
-			else if(i == 6)
-			{
-				wincondi[0]+=checkslot(positions,i);
-				wincondi[5]+=checkslot(positions,i);
-				wincondi[7]+=checkslot(positions,i);
-			}
-			else if(i == 7)
-			{
-				wincondi[1]+=checkslot(positions,i);
-				wincondi[5]+=checkslot(positions,i);
-			}
-			else if(i == 8)
-			{
-				wincondi[2]+=checkslot(positions,i);
-				wincondi[5]+=checkslot(positions,i);
-				wincondi[6]+=checkslot(positions,i);
+				case 0:
+					wincondi[0]+=checkslot(positions,i);
+					wincondi[3]+=checkslot(positions,i);
+					wincondi[6]+=checkslot(positions,i);
+					break;
+				case 1:
+					wincondi[1]+=checkslot(positions,i);
+					wincondi[3]+=checkslot(positions,i);
+					break;
+				case 2:
+					wincondi[2]+=checkslot(positions,i);
+					wincondi[3]+=checkslot(positions,i);
+					wincondi[7]+=checkslot(positions,i);
+					break;
+				case 3:
+					wincondi[0]+=checkslot(positions,i);
+					wincondi[4]+=checkslot(positions,i);
+					break;
+				case 4:
+					wincondi[1]+=checkslot(positions,i);
+					wincondi[4]+=checkslot(positions,i);
+					wincondi[6]+=checkslot(positions,i);
+					wincondi[7]+=checkslot(positions,i);
+					break;
+				case 5:
+					wincondi[2]+=checkslot(positions,i);
+					wincondi[4]+=checkslot(positions,i);
+					break;
+				case 6:
+					wincondi[0]+=checkslot(positions,i);
+					wincondi[5]+=checkslot(positions,i);
+					wincondi[7]+=checkslot(positions,i);
+					break;
+				case 7:
+					wincondi[1]+=checkslot(positions,i);
+					wincondi[5]+=checkslot(positions,i);
+					break;
+				case 8:
+					wincondi[2]+=checkslot(positions,i);
+					wincondi[5]+=checkslot(positions,i);
+					wincondi[6]+=checkslot(positions,i);
+					break;
 			}
 		}
-		for(i = 0; i<8; i++) {if(wincondi[i]==3) GameCondition = 1; if(wincondi[i]==-3) GameCondition = 2;} //check if any condition = 3 or -3
+		for(i = 0; i<8; i++) 
+			{if(wincondi[i]==3) GameCondition = 1; if(wincondi[i]==-3) GameCondition = 2;} //check if any condition = 3 or -3
 		if((turns == 9)&(GameCondition == 0)&(GameCondition == 0)) GameCondition = 4; // if no one won by tuen 9 it is a draw
 	}
 	else if((GameCondition == 1)||(GameCondition == 2)||(GameCondition == 4)) //victory or draw, 1 for player one 2 for player 2 4 for draw
