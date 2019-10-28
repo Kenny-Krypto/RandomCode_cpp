676c21d00c0926c5fbfd3a9ed458a411b4c63a25
Kenny K. Bui <Kenny.Bui@rutgers.edu>
2019-10-28 16:30:17 -0400

    Update fibR.cpp

--- Fib/fibR.cpp
+++ Fib/fibR.cpp
@@ -7,7 +7,7 @@ if(x < 0){ // IF Negative number
 	return 1;
 }
 if(x > 2){ // IF greater then 2
-	std::cout << x << " ";
+	//std::cout << x << " "; //Test
 	return fibR(x-1) + fibR(x-2); // used recursively to calcuate the value
 }
 if(x == 2)  // IF Equal to 2
